ac25c9044819cdb5d9bc4f8cae676898bf8f05e7
neosbob <neosbob@gmail.com>
2019-09-19 00:39:30 -0700

    Resolved Unit Tests Errors
     Resolved Unit Test Errors regarding Condition_add always returning false.

--- app/src/test/java/edu/fullerton/AdvSofwareProcess/InsertionSortApp/SortTest.java
+++ app/src/test/java/edu/fullerton/AdvSofwareProcess/InsertionSortApp/SortTest.java
@@ -6,19 +6,19 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SortTest {
 
-    @Mock
-    sort mockSort;
-
+    sort sortTest = new sort();
 
     @Test
     public void Condition_add_tooSmall() {
+
         boolean expectedResult = false;
 
-        boolean actualResult = mockSort.Condition_add(-1);
+        boolean actualResult = sortTest.Condition_add(-1);
 
         assertEquals(expectedResult,actualResult);
     }
@@ -27,9 +27,47 @@ public class SortTest {
     public void Condition_add_tooBig() {
         boolean expectedResult = false;
 
-        boolean actualResult = mockSort.Condition_add(10);
+        boolean actualResult = sortTest.Condition_add(10);
 
         assertEquals(expectedResult,actualResult);
     }
+
+    @Test
+    public void Condition_add_inRange() {
+        boolean expectedResult = true;
+
+        boolean actualResult = sortTest.Condition_add(4);
+
+
+
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void Condition_add_boundarytest_0() {
+        boolean expectedResult = true;
+
+        boolean actualResult = sortTest.Condition_add(0);
+
+        assertEquals(expectedResult,actualResult);
+    }
+    @Test
+    public void Condition_add_boundarytest_9() {
+        boolean expectedResult = true;
+
+        boolean actualResult = sortTest.Condition_add(9);
+
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void Check_Size_arraySizeinRange() {
+        boolean expectedResult = false;
+
+        boolean actualResult = sortTest.Condition_add(0);
+
+        assertEquals(expectedResult,actualResult);
+    }
+
 
 }
